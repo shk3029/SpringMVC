91e1fd8b6cf97ed40ccb563846dcf18eb5769e70
shk3029 <shk3029>
2019-02-17 14:11:11 +0900

    8강~12강 예제 커밋

--- src/main/java/me/js/HelloController.java
+++ src/main/java/me/js/HelloController.java
@@ -1,17 +1,25 @@
 package me.js;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+//@RestController
+@Controller
 public class HelloController {
 
     @Autowired
     HelloService helloService;
 
     @GetMapping("/hello")
+    @ResponseBody
     public String hello() {
         return "hello" + helloService.getName();
+    }
+
+    @GetMapping("/sample")
+    public String sample() {
+        return "sample";
     }
 }
--- src/main/java/me/js/WebConfig.java
+++ src/main/java/me/js/WebConfig.java
@@ -1,10 +1,43 @@
 package me.js;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan
 public class WebConfig {
+
+    @Bean
+    public HandlerMapping handlerMapping() {
+        RequestMappingHandlerMapping handlerMapping = new RequestMappingHandlerMapping();
+        handlerMapping.setInterceptors();// 핸들러 인터셉터를 통해 서블릿보다 좀 더 유연하게 다룰 수 있음
+        handlerMapping.setOrder(Ordered.HIGHEST_PRECEDENCE); // 여러 핸들러가 있을 때, 우선순위를 줄 수 있음
+        return handlerMapping;
+    }
+
+    @Bean
+    public HandlerAdapter handlerAdapter() {
+        RequestMappingHandlerAdapter handlerAdapter = new RequestMappingHandlerAdapter();
+        return handlerAdapter;
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
 }
